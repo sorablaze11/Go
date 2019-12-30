2788678e3198405887254c56ec8a7b806a8b0635
Sukh Raj Limbu <limburaj65@gmail.com>
2019-12-30 13:52:50 +0530

    Go server with redis

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    // "fmt"
+    "net/http"
+    "github.com/gorilla/mux"
+    "github.com/go-redis/redis"
+    "html/template"
+)
+
+var client *redis.Client
+var templates *template.Template
+
+func main(){
+    client = redis.NewClient(&redis.Options{
+        Addr : "localhost:6379",
+    })
+    templates = template.Must(template.ParseGlob("templates/*.html"))
+    r := mux.NewRouter()
+    r.HandleFunc("/", indexHandler).Methods("GET")
+    http.Handle("/", r)
+	http.ListenAndServe(":8080", nil)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+    comments, err := client.LRange("comments", 0, 10).Result()
+	templates.ExecuteTemplate(w, "index.html", nil)
+}
